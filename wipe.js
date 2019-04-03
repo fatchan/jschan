7a9008249c8da82399527fd3e45b63e888017272
fatchan <tom@69420.me>
2019-04-03 15:01:40 +0000

    update wipe script to add dummy admin account

--- wipe.js
+++ wipe.js
@@ -12,14 +12,19 @@ const Mongo = require(__dirname+'/helpers/db.js')
 	await Mongo.connect();
 	const Boards = require(__dirname+'/db-models/boards.js')
 		, Posts = require(__dirname+'/db-models/posts.js')
-		, Trips = require(__dirname+'/db-models/trips.js');
-	console.log('connected! wiping data')
-	await Boards.deleteIncrement('pol');
-	await Boards.deleteIncrement('b');
+		, Trips = require(__dirname+'/db-models/trips.js')
+		, Accounts = require(__dirname+'/db-models/accounts.js');
+	console.log('deleting accounts')
+	await Accounts.deleteAll();
+	console.log('deleting posts')
 	await Posts.deleteAll('pol');
 	await Posts.deleteAll('b');
+	console.log('deleting boards')
+	await Boards.deleteIncrement('pol');
+	await Boards.deleteIncrement('b');
 	await Boards.deleteAll();
 	await Trips.deleteAll();
+	console.log('adding b and pol')
 	await Boards.insertOne({
 		 _id: 'pol',
 		 name: 'Politically Incorrect',
@@ -30,6 +35,7 @@ const Mongo = require(__dirname+'/helpers/db.js')
 		 name: 'Random',
 		 description: 'post anything here',
 	})
+	console.log('creating indexes')
 	await Posts.db.collection('b').createIndex({"thread": 1});
 	await Posts.db.collection('pol').createIndex({"thread": 1});
 	await readdir('static/img/').then(async files => {
@@ -37,7 +43,7 @@ const Mongo = require(__dirname+'/helpers/db.js')
 			unlink(path.join('static/img/', file));
 		}))
 	});
-	console.log('done')
-	return;
+	console.log('creating admin account: admin:changeme');
+	await Accounts.insertOne('admin', 'changeme', 3);
+	console.log('done');
 })();
-
