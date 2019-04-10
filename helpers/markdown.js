cbc7135c906a4b6507de7b00aa18ba342a9a3544
fatchan <tom@69420.me>
2019-04-10 16:14:38 +0000

    different way of handling post styling. also added links

--- helpers/markdown.js
+++ helpers/markdown.js
@@ -1,62 +1,57 @@
 'use strict';
 
 const Posts = require(__dirname+'/../db-models/posts.js')
-	, quoteRegex = /^>>\d+/g
-	, greentextRegex = /^>[^>].+/g
-	, redtextRegex = /^<[^<].+/g
-	, boldRegex = /==.+==/g
-	, italicRegex = /__.+__/g
-	, spoilerRegex = /\|.+\|/g;
+	, quoteRegex = /^>>\d+$/gm
+	, greentextRegex = /^>[^>].+$/gm
+	, redtextRegex = /^<[^<].+$/gm
+	, boldRegex = /==.+==/gm
+	, italicRegex = /__.+__/gm
+	, linkRegex = /https?\:\/\/[^\s]+/g
+	, spoilerRegex = /\|.+\|/gm;
 
 module.exports = (board, thread, text) => {
 
-	const lines = text.split('\n')
+	//redtext
+	text = text.replace(redtextRegex, (match) => {
+		const red = match.substring(1);
+		return `<span class='redtext'>&lt;${red}</span>`;
+	});
 
-	for(let j = 0; j < lines.length; j++) {
-		//replace quotes
-		const quote = lines[j].match(quoteRegex);
-		if (quote) {
-			const quotenum = quote[0].substring(2);
-			lines[j] = lines[j].replace(quote[0], `<a class='quote' href='/${board}/thread/${thread}#${quotenum}'>&gt;&gt;${quotenum}</a>`);
-			continue;
-		}
-		//replace greentexts
-		const greentext = lines[j].match(greentextRegex);
-		if (greentext) {
-			const green = greentext[0].substring(1);
-			lines[j] = lines[j].replace(greentext[0], `<span class='greentext'>&gt;${green}</span>`);
-			continue;
-		}
-		//replace redtexts
-		const redtext = lines[j].match(redtextRegex);
-		if (redtext) {
-			const red = redtext[0].substring(1);
-			lines[j] = lines[j].replace(redtext[0], `<span class='redtext'>&lt;${red}</span>`);
-			continue;
-		}
-		//replace bolds
-		const boldtext = lines[j].match(boldRegex);
-		if (boldtext) {
-			const bold = boldtext[0].substring(2, boldtext[0].length-2);
-			lines[j] = lines[j].replace(boldtext[0], `<strong>${bold}</strong>`);
-			continue;
-		}
-		//replace italics
-		const italictext = lines[j].match(italicRegex);
-		if (italictext) {
-			const italic = italictext[0].substring(2, italictext[0].length-2);
-			lines[j] = lines[j].replace(italictext[0], `<em>${italic}</em>`);
-			continue;
-		}
-		//replace spoilers
-		const spoilertext = lines[j].match(spoilerRegex);
-		if (spoilertext) {
-			const spoiler = spoilertext[0].substring(1, spoilertext[0].length-1);
-			lines[j] = lines[j].replace(spoilertext[0], `<span class='spoiler'>${spoiler}</span>`);
-			continue;
-		}
-	}
+	//greentext
+	text = text.replace(greentextRegex, (match) => {
+		const green = match.substring(1);
+		return `<span class='greentext'>&gt;${green}</span>`;
+	});
 
-	return lines.join('\n');
+	//links
+	text = text.replace(linkRegex, (match) => {
+		return `<a href="${match}">${match}</a>`;
+	});
+
+	//quotes
+	text = text.replace(quoteRegex, (match) => {
+		const quotenum = match.substring(2);
+		return `<a class='quote' href='/${board}/thread/${thread}#${quotenum}'>&gt;&gt;${quotenum}</a>`;
+	});
+
+	//bold
+	text = text.replace(boldRegex, (match) => {
+		const bold = match.substring(2, match.length-2);
+		return `<strong>${bold}</strong>`;
+	});
+
+	//italic
+	text = text.replace(italicRegex, (match) => {
+		const italic = match.substring(2, match.length-2);
+		return `<italic>${italic}</em>`;
+	});
+
+	//spoilers
+	text = text.replace(spoilerRegex, (match) => {
+		const spoiler = match.substring(1, match.length-1);
+		return `<span class='spoiler'>${spoiler}</span>`;
+	});
+
+	return text;
 
 }
