9aff8a60312f036d3559b3519f3b8a4fad93236d
fatchan <tom@69420.me>
2019-05-16 10:09:38 +0000

    cache compiled function for templates

--- helpers/render.js
+++ helpers/render.js
@@ -7,6 +7,6 @@ const outputFile = require('fs-extra').outputFile
 	, templateDirectory = path.join(__dirname+'/../views/pages/');
 
 module.exports = async (htmlName, templateName, options) => {
-	const html = pug.renderFile(`${templateDirectory}${templateName}`, options);
+	const html = pug.renderFile(`${templateDirectory}${templateName}`, { ...options, cache: true });
 	return outputFile(`${uploadDirectory}html/${htmlName}`, html);
 };
