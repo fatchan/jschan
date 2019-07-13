d5a5a8d8c6d86afd93b723f7d8980f1ff478967e
fatchan <tom@69420.me>
2019-07-13 16:02:02 +0100

    cross quotes work on their own, and optional last slash in crossquote regex

--- helpers/posting/quotes.js
+++ helpers/posting/quotes.js
@@ -3,7 +3,7 @@
 const Posts = require(__dirname+'/../../db/posts.js')
 	, Boards = require(__dirname+'/../../db/boards.js')
 	, quoteRegex = />>\d+/g
-	, crossQuoteRegex = />>>\/\w+\/\d*$/gm;
+	, crossQuoteRegex = />>>\/\w+(?:\/\d*)?/gm;
 
 module.exports = async (board, text, thread) => {
 
@@ -15,11 +15,12 @@ module.exports = async (board, text, thread) => {
 	}
 
 	//make query for db including crossquotes
-	const queryOrs = []
+	const postQueryOrs = []
+	const boardQueryIns = []
 	const crossQuoteMap = {};
 	if (quotes) {
 		const quoteIds = [...new Set(quotes.map(q => +q.substring(2)))]; //only uniques
-		queryOrs.push({
+		postQueryOrs.push({
 			'board': board,
 			'postId': {
 				'$in': quoteIds
@@ -38,40 +39,53 @@ module.exports = async (board, text, thread) => {
 			if (!crossQuoteMap[crossQuoteBoard]) {
 				crossQuoteMap[crossQuoteBoard] = [];
 			}
-			crossQuoteMap[crossQuoteBoard].push(crossQuotePostId);
+			if (!isNaN(crossQuotePostId)) {
+				crossQuoteMap[crossQuoteBoard].push(crossQuotePostId);
+			}
 		}
 		const crossQuoteBoards = Object.keys(crossQuoteMap)
 		for (let i = 0; i < crossQuoteBoards.length; i++) {
 			const crossQuoteBoard = crossQuoteBoards[i];
 			const crossQuoteBoardPostIds = crossQuoteMap[crossQuoteBoard];
-			queryOrs.push({
-				'board': crossQuoteBoard,
-				'postId': {
-					'$in': crossQuoteBoardPostIds
-				}
-			})
+			if (crossQuoteBoardPostIds.length > 0) {
+				postQueryOrs.push({
+					'board': crossQuoteBoard,
+					'postId': {
+						'$in': crossQuoteBoardPostIds
+					}
+				});
+			} else {
+				boardQueryIns.push(crossQuoteBoard);
+			}
 		}
 	}
 
 	//get all the posts from quotes
 	const postThreadIdMap = {};
-	if (queryOrs.length > 0) {
-		const posts = await Posts.getPostsForQuotes(queryOrs);
-		//if none of the quotes were real, dont do a replace
-		if (posts.length === 0) {
-			return { quotedMessage: text, threadQuotes: [] };
+	const [ posts, boards ] = await Promise.all([
+		postQueryOrs.length > 0 ? Posts.getPostsForQuotes(postQueryOrs) : [],
+		boardQueryIns.length > 0 ? Boards.db.find({ '_id': { '$in': boardQueryIns } }, { projection: { '_id': 1 } }).toArray() : []
+	]);
+	//if none of the quotes were real, dont do a replace
+	if (posts.length === 0 && boards.length === 0) {
+		return { quotedMessage: text, threadQuotes: [] };
+	}
+	//turn the result into a map of postId => threadId/postId
+	for (let i = 0; i < posts.length; i++) {
+		const post = posts[i];
+		if (!postThreadIdMap[post.board]) {
+			postThreadIdMap[post.board] = {};
 		}
-		//turn the result into a map of postId => threadId/postId
-		for (let i = 0; i < posts.length; i++) {
-			const post = posts[i];
-			if (!postThreadIdMap[post.board]) {
-				postThreadIdMap[post.board] = {};
-			}
-			postThreadIdMap[post.board][post.postId] = {
-				'_id': post._id,
-				'thread': post.thread || post.postId,
-				'postId': post.postId
-			};
+		postThreadIdMap[post.board][post.postId] = {
+			'_id': post._id,
+			'thread': post.thread || post.postId,
+			'postId': post.postId
+		};
+	}
+	for (let i = 0; i < boards.length; i++) {
+		const boardName = boards[i]._id;
+		if (!postThreadIdMap[boardName]) {
+			postThreadIdMap[boardName] = {};
 		}
 	}
 
@@ -89,16 +103,17 @@ module.exports = async (board, text, thread) => {
 			return match;
 		});
 	}
-
 	if (crossQuotes) {
 		text = text.replace(crossQuoteRegex, (match) => {
 			const quote = match.split('/');
 			const quoteboard = quote[1];
 			const quotenum = +quote[2];
-			if (postThreadIdMap[quoteboard] && postThreadIdMap[quoteboard][quotenum]) {
-				return `<a class='quote' href='/${quoteboard}/thread/${postThreadIdMap[quoteboard][quotenum].thread}.html#${quotenum}'>&gt;&gt;&gt;/${quoteboard}/${quotenum}</a>`;
-			} else if (!quote[2]) {
-				return `<a class='quote' href='/${quoteboard}/index.html'>&gt;&gt;&gt;/${quoteboard}/</a>`;
+			if (postThreadIdMap[quoteboard]) {
+				if (!isNaN(quotenum) && postThreadIdMap[quoteboard][quotenum]) {
+					return `<a class='quote' href='/${quoteboard}/thread/${postThreadIdMap[quoteboard][quotenum].thread}.html#${quotenum}'>&gt;&gt;&gt;/${quoteboard}/${quotenum}</a>`;
+				} else {
+					return `<a class='quote' href='/${quoteboard}/index.html'>&gt;&gt;&gt;/${quoteboard}/</a>`;
+				}
 			}
 			return match;
 		});
