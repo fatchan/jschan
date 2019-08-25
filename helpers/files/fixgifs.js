6870a23bb2ce906443cf1ecc3afa2e582d39b525
fatchan <tom@69420.me>
2019-08-25 14:04:44 +0000

    move gif fix to separate file to nto do in unnecessary situation eng. audio upload, preparing for more accepted file types

---
+++ helpers/files/fixgifs.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = (file) => {
+	//handle gifs with multiple geometry and size
+	if (Array.isArray(file.geometry)) {
+		file.geometry = file.geometry[0];
+	}
+	if (Array.isArray(file.sizeString)) {
+		file.sizeString = file.sizeString[0];
+	}
+	if (Array.isArray(file.geometryString)) {
+		file.geometryString = file.geometryString[0];
+	}
+	return file;
+}
