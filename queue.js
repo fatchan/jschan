e2d1bb5768c874ee74323e92551ac261306cb50c
fatchan <tom@69420.me>
2019-08-31 10:05:22 +0000

    allow optiosn for queue for cron scheduling, etc

--- queue.js
+++ queue.js
@@ -4,6 +4,6 @@ const Queue = require('bull')
 	, configs = require(__dirname+'/configs/main.json')
 	, generateQueue = new Queue('generate', { 'redis': configs.redis });
 
-module.exports.push = (data) => {
-	generateQueue.add(data);
+module.exports.push = (data, options) => {
+	generateQueue.add(data, options);
 }
