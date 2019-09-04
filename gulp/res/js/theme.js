b85afbb0db820c0854c023cda4ad287f1cb53624
fatchan <tom@69420.me>
2019-09-04 17:07:53 +0000

    default theme option and slight improvement to head template

--- gulp/res/js/theme.js
+++ gulp/res/js/theme.js
@@ -1,11 +1,20 @@
 function changeTheme(e) {
 	var theme = e ? this.value : localStorage.getItem('theme');
 	if (!theme) {
-		return;
+		theme = 'default';
 	}
-	localStorage.setItem('theme', theme)
+	localStorage.setItem('theme', theme);
 	var themeLink = document.getElementById('theme');
-	themeLink.href = '/css/themes/'+theme+'.css';
+	//if somebody makes a theme file named "default", it wont work. so just dont.
+	if (theme === 'default') {
+		var defaultTheme = themeLink.dataset.theme;
+		var defaultHref = '/css/themes/'+defaultTheme+'.css';
+		if (themeLink.href !== defaultHref) {
+			themeLink.href = '/css/themes/'+defaultTheme+'.css';
+		}
+	} else {
+		themeLink.href = '/css/themes/'+theme+'.css';
+	}
 }
 
 changeTheme();
