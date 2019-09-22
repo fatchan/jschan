354ea5297d1d0d7003abd8c94aa22f011ee78424
fatchan <tom@69420.me>
2019-09-22 10:43:19 +0000

    number to title when new posts, and highlight until fully scrolled into view

---
+++ gulp/res/js/titlescroll.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+	let focused = false;
+	let unread = [];
+	const originalTitle = document.title;
+
+	const isVisible = (e) => {
+		const top = e.getBoundingClientRect().top;
+		const bottom = e.getBoundingClientRect().bottom;
+		const height = window.innerHeight;
+		return (top >= 0 || bottom-top > height) && bottom <= height;
+	}
+
+	const updateTitle = () => {
+		if (unread.length === 0) {
+			document.title = originalTitle;
+		} else {
+			document.title = `(${unread.length}) ${originalTitle}`;
+		}
+	}
+
+	const focusChange = () => {
+		focused = !focused;
+	}
+
+	const updateVisible = () => {
+		unread = unread.filter(p => {
+			if (isVisible(p)) {
+				p.classList.remove('highlighted');
+				return false;
+			}
+			return true;
+		});
+		updateTitle();
+	}
+
+	window.onfocus = () => {
+		focusChange();
+		updateVisible();
+	}
+	window.onblur = focusChange;
+	window.addEventListener('scroll', updateVisible);
+
+
+	window.addEventListener('addPost', function(e) {
+		const post = e.detail;
+		//if browsing another tab or the post is out of scroll view
+		if (!focused || !isVisible(post)) {
+			post.classList.add('highlighted');
+			unread.push(post);
+			updateTitle();
+		}
+	});
+
+
+});
