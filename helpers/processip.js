9276a4cc74e4b3288dddff4050219291683e8ed1
fatchan <tom@69420.me>
2019-10-04 13:30:10 +0000

    handle ipv6 in processiphelper references #2

--- helpers/processip.js
+++ helpers/processip.js
@@ -5,11 +5,12 @@ const { ipHashSecret } = require(__dirname+'/../configs/main.json')
 
 module.exports = (req, res, next) => {
 	const ip = req.headers['x-real-ip']; //need to consider forwarded-for, etc here and in nginx
-	const split = ip.split('.');
+	const delimiter = ip.includes('.') ? '.' : ':';
+	let split = ip.split(delimiter);
 	res.locals.ip = {
 		hash: createHash('sha256').update(ipHashSecret + ip).digest('base64'),
-		qrange: createHash('sha256').update(ipHashSecret + split.slice(0,3).join('.')).digest('base64'),
-		hrange: createHash('sha256').update(ipHashSecret + split.slice(0,2).join('.')).digest('base64'),
+		qrange: createHash('sha256').update(ipHashSecret + split.slice(0,Math.floor(split.length*0.75)).join(delimiter)).digest('base64'),
+		hrange: createHash('sha256').update(ipHashSecret + split.slice(0,Math.floor(split.length*0.5)).join(delimiter)).digest('base64'),
 	}
 	next();
 }
