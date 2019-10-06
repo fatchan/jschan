d8b3a79e5d9d1cc0a8cb3e80e9c29102930b416d
fatchan <tom@69420.me>
2019-10-06 12:22:19 +0000

    significantly faster image identify, just use the first frame from gifs with [0]

--- helpers/files/imageidentify.js
+++ helpers/files/imageidentify.js
@@ -5,7 +5,8 @@ const gm = require('gm')
 module.exports = (filename, folder, temp) => {
 
 	return new Promise((resolve, reject) => {
-		gm(temp === true ? filename : `${uploadDirectory}/${folder}/${filename}`)
+		const filePath = temp === true ? filename : `${uploadDirectory}/${folder}/${filename}`;
+		gm(`${filePath}[0]`) //0 for first frame of gifs, much faster
 			.identify(function (err, data) {
 				if (err) {
 					return reject(err);
