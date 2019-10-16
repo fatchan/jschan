f3a203fba933e0ddad0334a11feebb114ba2bd22
fatchan <tom@69420.me>
2019-10-16 02:55:34 +0000

    remove useless semantics elements

--- helpers/timeutils.js
+++ helpers/timeutils.js
@@ -21,27 +21,33 @@ module.exports = {
 		const difference = currentTime.getTime() - eventTime.getTime();
 		let amount = 0;
 		let ret = '';
+		let color;
 		if (difference < MINUTE) {
-			return 'Now';
+			return { color: '#39d6bc', text:'Now' };
 		} else if (difference < HOUR) {
 			amount = Math.floor(difference / MINUTE);
+			color = '#008000';
 			ret += `${amount} minute`
 		} else if (difference < DAY) {
 			amount = Math.floor(difference / HOUR);
+			color = '#84c100';
 			ret += `${amount} hour`
 		} else if (difference < WEEK) {
 			amount = Math.floor(difference / DAY);
+			color = '#fffd00';
 			ret += `${amount} day`;
 		} else if (difference < MONTH) {
 			amount = Math.floor(difference / WEEK);
+			color = '#ff6700';
 			ret += `${amount} week`;
 		} else if (difference < YEAR) {
 			amount = Math.floor(difference / MONTH);
+			color = '#ff0000';
 			ret += `${amount} month`;
 		} else {
-			return 'More than a year ago';
+			return { color: '#ff000047', text: 'More than a year ago' };
 		}
-		return `${ret}${amount > 1 ? 's' : ''} ago`;
+		return { color, text: `${ret}${amount > 1 ? 's' : ''} ago` };
 	}
 
 };
