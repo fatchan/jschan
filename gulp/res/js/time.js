c71f60fdf4e0cdb5caf11f67425e80dbc27d2ff7
fatchan <tom@69420.me>
2019-11-30 23:01:08 +1100

    relative time, toggle without refresh and make local not optional references #57

--- gulp/res/js/time.js
+++ gulp/res/js/time.js
@@ -1,48 +1,105 @@
-!localStorage.getItem('localtime') ? setLocalStorage('localtime', true) : void 0;
-!localStorage.getItem('hour12') ? setLocalStorage('hour12', false) : void 0;
+!localStorage.getItem('relative') ? setLocalStorage('relative', false) : void 0;
+let relativeTime = localStorage.getItem('relative') == 'true';
 
-let localTime = localStorage.getItem('localtime') == 'true';
-let hour12 = localStorage.getItem('hour12') == 'true';
+!localStorage.getItem('24hour') ? setLocalStorage('24hour', false) : void 0;
+let hour24 = localStorage.getItem('24hour') == 'true';
 
-const makeDateLocal = (date) => {
-	date.innerText = new Date(date.dateTime).toLocaleString(0, {hour12: !hour12});
+let dates = [];
+const dateElems = document.getElementsByClassName('post-date');
+for (let i = 0; i < dateElems.length; i++) {
+	dates.push(dateElems[i]); //convert to array
 }
 
-if (localTime) {
-	const dates = document.getElementsByClassName('post-date');
-	for (let i = 0; i < dates.length; i++) {
-		makeDateLocal(dates[i]);
+const YEAR = 31536000000
+	, MONTH = 2592000000
+	, WEEK = 604800000
+	, DAY = 86400000
+	, HOUR = 3600000
+	, MINUTE = 60000;
+
+const relativeTimeString = (date) => {
+    const difference = Date.now() - new Date(date).getTime();
+    let amount = 0;
+    let ret = '';
+    if (difference < MINUTE) {
+        return 'Now';
+    } else if (difference < HOUR) {
+        amount = Math.floor(difference / MINUTE);
+        ret = `${amount} minute`;
+    } else if (difference < DAY) {
+        amount = Math.floor(difference / HOUR);
+        ret = `${amount} hour`;
+    } else if (difference < WEEK) {
+        amount = Math.floor(difference / DAY);
+        ret = `${amount} day`;;
+    } else if (difference < MONTH) {
+        amount = Math.floor(difference / WEEK);
+        ret = `${amount} week`;
+    } else if (difference < YEAR) {
+        amount = Math.floor(difference / MONTH);
+        ret = `${amount} month`;
+    } else {
+        return '> 1 year ago';
+    }
+    return `${ret}${amount > 1 ? 's' : ''} ago`;
+}
+
+const changeDateFormat = (date) => {
+	const dateString = new Date(date.dateTime).toLocaleString(0, {hour12: !hour24});
+	if (relativeTime) {
+		date.innerText = relativeTimeString(date.dateTime);
+		date.title = dateString;
+	} else {
+		date.innerText = dateString;
+		date.title = '';
 	}
 }
+
+const updateDates = () => {
+	for (let i = 0; i < dates.length; i++) {
+		changeDateFormat(dates[i]);
+	}
+}
+
+updateDates();
 
 window.addEventListener('settingsReady', function(event) {
 
-	const timeSetting = document.getElementById('time-setting');
-	const hour12Setting = document.getElementById('hour12-setting');
+	let relativeInterval = relativeTime ? setInterval(updateDates, MINUTE) : void 0;
 
-	const toggleLocalTime = () => {
-		localTime = !localTime;
-		setLocalStorage('localtime', localTime);
-		console.log('toggling local time', localTime);
+	const hour24Setting = document.getElementById('24hour-setting');
+	const togglehour24 = () => {
+		hour24 = !hour24;
+		setLocalStorage('24hour', hour24);
+		updateDates();
+		console.log('toggling 24h time', hour24);
 	}
-	timeSetting.checked = localTime;
-	timeSetting.addEventListener('change', toggleLocalTime, false);
+	hour24Setting.checked = hour24;
+	hour24Setting.addEventListener('change', togglehour24, false);
 
-	const toggleHour12 = () => {
-		hour12 = !hour12;
-		setLocalStorage('hour12', hour12);
-		console.log('toggling 24h time', hour12);
+	const relativeTimeSetting = document.getElementById('relative-setting');
+	const togglerelativeTime = () => {
+		relativeTime = !relativeTime;
+		setLocalStorage('relative', relativeTime);
+		updateDates();
+		if (relativeTime) {
+			relativeInterval = setInterval(updateDates, MINUTE);
+		} else {
+			clearInterval(relativeInterval);
+		}
+		console.log('toggling relative time', relativeTime);
 	}
-	hour12Setting.checked = hour12;
-	hour12Setting.addEventListener('change', toggleHour12, false);
+	relativeTimeSetting.checked = relativeTime;
+	relativeTimeSetting.addEventListener('change', togglerelativeTime, false);
 
 });
 
 window.addEventListener('addPost', function(e) {
 
 	const date = e.detail.post.querySelector('.post-date');
-	if (localTime) {
-		makeDateLocal(date);
+	if (!e.detail.hover) {
+		dates.push(date);
 	}
+	changeDateFormat(date);
 
 });
