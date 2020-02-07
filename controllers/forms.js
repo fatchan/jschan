f540d07e67882353f7e34802ee98128e83e5fddf
fatchan <tom@69420.me>
2020-02-07 21:26:29 +1100

    option to disable filename filter

--- controllers/forms.js
+++ controllers/forms.js
@@ -3,7 +3,7 @@
 const express  = require('express')
 	, router = express.Router()
 	, Boards = require(__dirname+'/../db/boards.js')
-	, { globalLimits, debugLogs } = require(__dirname+'/../configs/main.js')
+	, { globalLimits, debugLogs, safeFileNames } = require(__dirname+'/../configs/main.js')
 	//middlewares
 	, calcPerms = require(__dirname+'/../helpers/checks/calcpermsmiddleware.js')
 	, hasPerms = require(__dirname+'/../helpers/checks/haspermsmiddleware.js')
@@ -27,7 +27,7 @@ const express  = require('express')
 	, postFiles = upload({
 		debug: debugLogs,
 		createParentPath: true,
-		safeFileNames: /[^\w\s-]+/g,
+		safeFileNames,
 		preserveExtension: 4,
 		limits: {
 			totalSize: globalLimits.postFilesSize.max,
@@ -41,7 +41,7 @@ const express  = require('express')
 	, bannerFiles = upload({
 		debug: debugLogs,
 		createParentPath: true,
-		safeFileNames: /[^\w\s-]+/g,
+		safeFileNames,
 		preserveExtension: 3,
 		limits: {
 			totalSize: globalLimits.bannerFilesSize.max,
@@ -75,6 +75,7 @@ const express  = require('express')
 
 //make new post
 router.post('/board/:board/post', dnsblCheck, sessionRefresh, Boards.exists, calcPerms, banCheck, postFiles, paramConverter, verifyCaptcha, numFiles, makePostController);
+//router.post('/board/:board/modpost', dnsblCheck, sessionRefresh, Boards.exists, calcPerms, banCheck, isLoggedIn, hasPerms(3), postFiles, paramConverter, csrf, numFiles, makePostController); //mod post has token instead of captcha
 
 //post actions
 router.post('/board/:board/actions', sessionRefresh, Boards.exists, calcPerms, banCheck, paramConverter, verifyCaptcha, actionController); //public, with captcha
