d04b391b7e99d15ea634bb8fab92596dbccd2067
fatchan <tom@69420.me>
2020-02-13 08:27:48 +0100

    patch brainlet filtering to make filters lowercase

--- models/forms/makepost.js
+++ models/forms/makepost.js
@@ -104,11 +104,11 @@ module.exports = async (req, res, next) => {
 		}
 		//global filters
 		if (globalSettings && globalSettings.filters.length > 0 && globalSettings.filterMode > 0) {
-			hitGlobalFilter = globalSettings.filters.some(filter => { return allContents.includes(filter) });
+			hitGlobalFilter = globalSettings.filters.some(filter => { return allContents.includes(filter.toLowerCase()) });
 		}
 		//board-specific filters
 		if (!hitGlobalFilter && res.locals.permLevel >= 4 && filterMode > 0 && filters && filters.length > 0) {
-			hitLocalFilter = filters.some(filter => { return allContents.includes(filter) });
+			hitLocalFilter = filters.some(filter => { return allContents.includes(filter.toLowerCase()) });
 		}
 		if (hitGlobalFilter || hitLocalFilter) {
 			await deleteTempFiles(req).catch(e => console.error);
