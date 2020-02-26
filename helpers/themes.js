53da09777d4a2adab1b1d8aae1afd9a1965ce06c
fatchan <tom@69420.me>
2020-02-26 13:40:40 +0100

    allow custom theme/codetheme list in settings andimprove makaba, renamed to clear

--- helpers/themes.js
+++ helpers/themes.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const { readdirSync } = require('fs');
+const { readdirSync } = require('fs')
+	, { themes, codeThemes } = require(__dirname+'/../configs/main.js');
 
 module.exports = {
 
-	themes: readdirSync(__dirname+'/../gulp/res/css/themes/').map(x => x.substring(0,x.length-4)),
+	themes: themes.length > 0 ? themes : readdirSync(__dirname+'/../gulp/res/css/themes/').map(x => x.substring(0,x.length-4)),
 
-	codeThemes: readdirSync(__dirname+'/../node_modules/highlight.js/styles/').map(x => x.substring(0,x.length-4)),
+	codeThemes: codeThemes.length > 0 ? codeThemes : readdirSync(__dirname+'/../node_modules/highlight.js/styles/').map(x => x.substring(0,x.length-4)),
 
 }
