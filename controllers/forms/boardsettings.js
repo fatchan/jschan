95ecb50a0688dcd8b37f1d9e80e607996f1526f5
fatchan <tom@69420.me>
2020-04-23 17:56:34 +1000

    options changes, make unlisted/webring unlisted not opposites make thread creation locking possible, and add it as a captcha option

--- controllers/forms/boardsettings.js
+++ controllers/forms/boardsettings.js
@@ -96,13 +96,16 @@ module.exports = async (req, res, next) => {
 		errors.push(`Max reply message length must be 0-${globalLimits.fieldLength.message} and not less than "Min Reply Message Length" (currently ${res.locals.board.settings.minReplyMessageLength})`);
 	}
 
+	if (typeof req.body.lock_mode === 'number' && (req.body.lock_mode < 0 || req.body.lock_mode > 2)) {
+		errors.push('Invalid lock mode');
+	}
 	if (typeof req.body.captcha_mode === 'number' && (req.body.captcha_mode < 0 || req.body.captcha_mode > 2)) {
 		errors.push('Invalid captcha mode');
 	}
 	if (typeof req.body.tph_trigger === 'number' && (req.body.tph_trigger < 0 || req.body.tph_trigger > 10000)) {
 		errors.push('Invalid tph trigger threshold');
 	}
-	if (typeof req.body.tph_trigger_action === 'number' && (req.body.tph_trigger_action < 0 || req.body.tph_trigger_action > 3)) {
+	if (typeof req.body.tph_trigger_action === 'number' && (req.body.tph_trigger_action < 0 || req.body.tph_trigger_action > 4)) {
 		errors.push('Invalid tph trigger action');
 	}
 	if (typeof req.body.filter_mode === 'number' && (req.body.filter_mode < 0 || req.body.filter_mode > 2)) {
---
+++ migrations/migration-0.0.4.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = async(db, redis) => {
+	console.log('Renaming some settings fields on boards');
+	await db.collection('boards').updateMany({}, {
+		'$rename': {
+			'settings.locked': 'settings.lockMode',
+			'settings.unlisted': 'settings.unlistedLocal',
+			'settings.webring': 'settings.unlistedWebring'
+		}
+	});
+	console.log('upadting renamed fields to proper values')
+	await db.collection('boards').updateMany({
+		'settings.locked': true,
+	}, {
+		'$set': {
+			'settings.lockMode': 2,
+		}
+	});
+	await db.collection('boards').updateMany({
+		'settings.locked': false,
+	}, {
+		'$set': {
+			'settings.lockMode': 0,
+		}
+	});
+	await db.collection('boards').updateMany({
+		'settings.triggerAction': 3,
+	}, {
+		'$set': {
+			'settings.triggerAction': 4,
+		}
+	});
+	console.log('clearing boards cache');
+	await redis.deletePattern('board:*')
+};
