7b3b416cd6c1efd5aadc3ab690d67b4451f252a2
fatchan <tom@69420.me>
2020-04-26 14:48:22 +1000

    add new migration

--- migrations/index.js
+++ migrations/index.js
@@ -5,4 +5,5 @@ module.exports = {
 	'0.0.2': require(__dirname+'/migration-0.0.2.js'), //rename ip field in posts
 	'0.0.3': require(__dirname+'/migration-0.0.3.js'), //move files from /img to /file/
 	'0.0.4': require(__dirname+'/migration-0.0.4.js'), //rename some fields for board lock mode and unlisting
+	'0.0.5': require(__dirname+'/migration-0.0.5.js'), //add bumplimit to board settings
 }
