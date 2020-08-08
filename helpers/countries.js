21e2d0c2e6ac0f62a2c083cf72b2ae0124ccc91b
Thomas Lynch <tom@69420.me>
2020-08-08 22:32:57 +1000

    Make countries helper support cloudflare XX and T1 country codes for unknown/tor exit node

--- helpers/countries.js
+++ helpers/countries.js
@@ -4,6 +4,9 @@ const countries = require('i18n-iso-countries')
 	, countryNamesMap = countries.getNames('en')
 	, countryCodes = Object.keys(countryNamesMap);
 
+countryNamesMap['XX'] = 'Unknown';
+countryNamesMap['T1'] = 'Tor Exit Node';
+
 module.exports = {
 	countryNamesMap,
 	countryCodes,
