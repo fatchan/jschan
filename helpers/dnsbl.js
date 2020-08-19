81fe9787dd620e57dfa285cfc5f8fb7a6e3843ff
Thomas Lynch <tom@69420.me>
2020-08-19 14:05:33 +0000

    no need for dnsbl on tor user

--- helpers/dnsbl.js
+++ helpers/dnsbl.js
@@ -3,12 +3,13 @@
 const cache = require(__dirname+'/../redis.js')
 	, dynamicResponse = require(__dirname+'/dynamic.js')
 	, { ipHeader, dnsbl } = require(__dirname+'/../configs/main.js')
+	, { isIP } = require('net')
 	, { batch } = require('dnsbl');
 
 module.exports = async (req, res, next) => {
 
-	if (dnsbl.enabled) {
-		const ip = req.headers[ipHeader] || req.connection.remoteAddress;
+	if (dnsbl.enabled && isIP(res.locals.ip.raw)) {
+		const ip = res.locals.ip.raw;
 		let isBlacklisted = await cache.get(`blacklisted:${ip}`);
 		if (isBlacklisted === null) { //not cached
 			const dnsblResp = await batch(ip, dnsbl.blacklists);
