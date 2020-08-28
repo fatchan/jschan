04b2b21d95bb0a2e20b5f7b74e9f9ea1e48b3ce0
some random guy <somerandomguy@rape.lol>
2020-08-28 21:10:04 +0200

    fix dice roll math
    In the old implementation, if you had for example 2 dice, you could only roll
    even numbers, which is clearly wrong.
    
    Also reduced the max numdice to 99 to not DoS the server with large numdice
    values.

--- helpers/posting/diceroll.js
+++ helpers/posting/diceroll.js
@@ -2,8 +2,8 @@
 
 module.exports = {
 
-	regexPrepare: /##(?<numdice>[1-9][0-9]{0,8})d(?<numsides>1[0-9]{1,8}|[2-9][0-9]{0,8})(?:(?<operator>[+-])(?<modifier>[1-9][0-9]{0,8}))?(?<value>=[1-9][0-9]{0,8})?/gmi,
-	regexMarkdown: /##(?<numdice>[1-9][0-9]{0,8})d(?<numsides>1[0-9]{1,8}|[2-9][0-9]{0,8})(?:(?<operator>[+-])(?<modifier>[1-9][0-9]{0,8}))?&#x3D;(?<value>[1-9][0-9]{0,8})/gmi,
+	regexPrepare: /##(?<numdice>[1-9][0-9]{0,1})d(?<numsides>1[0-9]{1,8}|[2-9][0-9]{0,8})(?:(?<operator>[+-])(?<modifier>[1-9][0-9]{0,8}))?(?<value>=[1-9][0-9]{0,8})?/gmi,
+	regexMarkdown: /##(?<numdice>[1-9][0-9]{0,1})d(?<numsides>1[0-9]{1,8}|[2-9][0-9]{0,8})(?:(?<operator>[+-])(?<modifier>[1-9][0-9]{0,8}))?&#x3D;(?<value>[1-9][0-9]{0,8})/gmi,
 
 	prepare: (force) => (match, numdice, numsides, operator, modifier, value) => {
 		if (!force && value) {
@@ -14,7 +14,10 @@ module.exports = {
 		numsides = parseInt(numsides);
 		let matchWithoutValue = match.replace(/=.*/, '');
 
-		let sum = (Math.floor(Math.random() * numsides) + 1) * numdice;
+		let sum = 0;
+		for (let i = 0; i < numdice; ++i) {
+			sum += Math.floor(Math.random() * numsides) + 1;
+		}
 		if (modifier && operator) {
 			modifier = parseInt(modifier);
 			//do i need to make sure it doesnt go negative or maybe give absolute value?
