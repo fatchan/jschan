0d609623e376ec131933e3517591f7a70a9b4df3
Thomas Lynch <tom@69420.me>
2020-09-12 11:12:55 +0000

    make haspermsmiddleware actually cache params

--- helpers/checks/haspermsmiddleware.js
+++ helpers/checks/haspermsmiddleware.js
@@ -4,7 +4,7 @@ const cache = {};
 
 module.exports = (requiredLevel) => {
 
-	return cache[requiredLevel] || (function(req, res, next) {
+	return cache[requiredLevel] || (cache[requiredLevel] = function(req, res, next) {
 		if (res.locals.permLevel > requiredLevel) {
 			return res.status(403).render('message', {
 				'title': 'Forbidden',
