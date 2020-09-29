98e091ef729df62b2c311257b270158e6d9c365d
Thomas Lynch <tom@69420.me>
2020-09-29 06:33:19 +0000

    close #251

--- gulp/res/js/hidefileinput.js
+++ gulp/res/js/hidefileinput.js
@@ -3,7 +3,7 @@ if (fileInput) {
 	//not using display: none because we still want to show the browser prompt for a "required" file
 	fileInput.style.position = 'absolute';
 	fileInput.style.border = 'none';
-	fileInput.style.height = '0';
-	fileInput.style.width = '0';
-	fileInput.style.opacity = '0';
+	fileInput.style.height = '1px';
+	fileInput.style.width = '1px';
+//	fileInput.style.opacity = '0';
 }
