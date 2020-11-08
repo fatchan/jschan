da8522faec1e1d0e9e5123900e051b15e9834f1e
some random guy <somerandomguy@rape.lol>
2020-11-08 16:51:13 +0100

    audio thumbnails

---
+++ helpers/files/audiothumbnail.js
@@ -0,0 +1,22 @@
+const ffmpeg = require('fluent-ffmpeg')
+	, { thumbSize } = require(__dirname+'/../../configs/main.js')
+	, uploadDirectory = require(__dirname+'/uploadDirectory.js');
+
+module.exports = (file) => {
+
+	return new Promise((resolve, reject) => {
+		ffmpeg(`${uploadDirectory}/file/${file.filename}`)
+		.on('end', () => {
+			return resolve();
+		})
+		.on('error', function(err, stdout, stderr) {
+			return reject(err);
+		})
+		.complexFilter([{
+			filter: 'showwavespic',
+			options: { split_channels: 1, s: `${thumbSize}x${thumbSize}` }
+		}])
+		.save(`${uploadDirectory}/file/thumb-${file.hash}${file.thumbextension}`);
+	});
+
+};
