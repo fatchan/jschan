32ec1152b46f753051b02c93bc08cba5a6aa687b
Thomas Lynch <tom@69420.me>
2021-01-21 19:08:29 +0000

    more 'tor' -> 'anonymizer' rewording changes including some conditionals, comments and FAQ page text close #316

--- helpers/checks/blockbypass.js
+++ helpers/checks/blockbypass.js
@@ -12,7 +12,7 @@ module.exports = async (req, res, next) => {
 	if (res.locals.preFetchedBypassId //if they already have a bypass
 		|| (!blockBypass.enabled //or if block bypass isnt enabled
 			&& (!blockBypass.forceAnonymizers //and we dont force it for anonymizer
-			|| !res.locals.anonymizer))) { //or they arent a .onion
+			|| !res.locals.anonymizer))) { //or they arent on an anonymizer
 		return next();
 	}
 
@@ -45,7 +45,7 @@ module.exports = async (req, res, next) => {
 
 	if (bypass //if they have a valid bypass
 		&& (bypass.uses < blockBypass.expireAfterUses //and its not overused
-			|| (res.locals.anonymizer && !blockBypass.forceOnion))) { //OR its disabled for .onion, which ignores usage check
+			|| (res.locals.anonymizer && !blockBypass.forceAnonymizers))) { //OR its forced for anonymizers
 		return next();
 	}
 
