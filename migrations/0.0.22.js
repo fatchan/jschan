92806c45783de6bd99351021076e3a468b419e17
Thomas Lynch <tom@69420.me>
2021-02-22 16:10:30 -0800

    Update 0.0.22.js

--- migrations/0.0.22.js
+++ migrations/0.0.22.js
@@ -5,7 +5,7 @@ const fs = require('fs-extra');
 module.exports = async(db, redis) => {
 	console.log('remove need for configs/webring');
 	const webring = require(__dirname+'/../configs/template.js.example');
-	const settings = await redis.get('globalsetings');
+	const settings = await redis.get('globalsettings');
 	const newSettings = { ...settings, ...webring };
 	redis.set('globalsettings', newSettings);
 };
