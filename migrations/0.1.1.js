0cce1026fbaed8cb0d12d5ecdf09c1c4eb9321a4
Thomas Lynch <tom@69420.me>
2021-03-12 10:48:55 +0000

    correct migration with something else

--- migrations/0.1.1.js
+++ migrations/0.1.1.js
@@ -3,11 +3,13 @@
 const fs = require('fs-extra');
 
 module.exports = async(db, redis) => {
-	console.log('adding flags customisation db entries');
+	console.log('adding flags customisation');
+	await fs.ensureDir(`${uploadDirectory}/flag/`);
 	const template = require(__dirname+'/../configs/template.js.example');
 	await db.collection('globalsettings').updateOne({ _id: 'globalsettings' }, {
 		'$set': {
 			'globalLimits.flagFiles': template.globalLimits.flagFiles,
+			'globalLimits.flagFilesSize': template.globalLimits.flagFilesSize,
 		}
 	});
 	await db.collection('boards').updateMany({}, {
