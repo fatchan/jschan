070d6939693b2142bc910c8358d9e999b007b097
Thomas Lynch <tom@69420.me>
2021-06-24 09:20:52 +0000

    try link labelling change for on site things, allow / start

--- helpers/posting/linkmatch.js
+++ helpers/posting/linkmatch.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const parenPairRegex = /\((?:[^)(]+|\((?:[^)(]+|\([^)(]\))*\))*\)/g
+//	, config = require(__dirname+'/../../config.js');
 
 module.exports = (permLevel, match, p1, p2, p3, offset, string, groups) => {
 	let { url, label, urlOnly } = groups
@@ -28,6 +29,17 @@ module.exports = (permLevel, match, p1, p2, p3, offset, string, groups) => {
 	}
 	url = url.replace(/\(/g, '%28')
 			.replace(/\)/g, '%29');
-	//TODO: if the link domain is one of the site domains, remove the domain and make it an absolute link, for users on different domains or anonymizers
+/*
+	//todo: Something to revisit later
+	const href = url;
+	if (urlOnly == null) {
+		try {
+			const urlObject = new URL(url);
+			if (config.get.allowedHosts.includes(urlObject.hostname)) {
+				href = `${urlObject.pathname}${urlObject.search}${urlObject.hash}`;
+			}
+		} catch (e) { }
+	}
+*/
 	return `<a rel='nofollow' referrerpolicy='same-origin' target='_blank' href='${url}'>${label || url}</a>${excess}`;
 };
