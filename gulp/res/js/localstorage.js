dfa410e86bef9ad79559ff43949587fcdcb7f991
Thomas Lynch <tom@69420.me>
2021-12-01 03:31:01 +0000

    script fixed

--- gulp/res/js/localstorage.js
+++ gulp/res/js/localstorage.js
@@ -77,3 +77,14 @@ if (!validHosts.some(h => h === location.hostname)) {
 	//fuck off
 }
 */
+
+/*
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/serviceWorker.js')
+		.then(function(registration) {
+			console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		}).catch(function(err) {
+			console.log('ServiceWorker registration failed: ', err);
+		});
+}
+*/
