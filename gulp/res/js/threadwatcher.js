3559e46aefac9075f5c94d0fc6a21c3b5c153047
Thomas Lynch <tom@69420.me>
2021-12-08 06:56:37 +1100

    more little things for 292, add/remove from list

--- gulp/res/js/threadwatcher.js
+++ gulp/res/js/threadwatcher.js
@@ -1,5 +1,5 @@
 function threadwatcher(locals) {var pug_html = "", pug_mixins = {}, pug_interp;pug_mixins["threadwatcher"] = pug_interp = function(){
 var block = (this && this.block), attributes = (this && this.attributes) || {};
-pug_html = pug_html + "\u003Cdiv class=\"flex-center\" id=\"threadwatcher\"\u003E\u003Cdiv class=\"row\"\u003E\u003Cdiv class=\"noselect\" id=\"threadwatcher-dragHandle\"\u003EThread Watcher\u003C\u002Fdiv\u003E\u003C\u002Fdiv\u003E\u003C\u002Fdiv\u003E";
+pug_html = pug_html + "\u003Cdiv class=\"flex-center\" id=\"threadwatcher\"\u003E\u003Cdiv class=\"row noselect\" id=\"threadwatcher-dragHandle\"\u003EThread Watcher\u003C\u002Fdiv\u003E\u003C\u002Fdiv\u003E";
 };
 pug_mixins["threadwatcher"]();;return pug_html;}
--- gulp/res/js/watchlist.js
+++ gulp/res/js/watchlist.js
@@ -7,8 +7,12 @@ class ThreadWatcher {
 		this.clearButton = document.getElementById('watchlist-clear');
 		this.clearButton.addEventListener('click', this.clear, false);
 		this.createListHtml();
+		this.threadWatcher = document.getElementById('threadwatcher');
+		//show this time in draghandle? could also add .spin (same as captcha refresh) when updating
 		this.refreshInterval = setInterval(this.refresh, 60 * 1000);
 		this.refresh();
+
+		this.add()
 	}
 
 	refresh() {
@@ -25,25 +29,34 @@ class ThreadWatcher {
 		const footer = document.getElementById('bottom');
 		footer.insertAdjacentHTML('afterend', threadWatcherHtml);
 		new Dragable('#threadwatcher-dragHandle', '#threadwatcher');
+		//todo: add() all ones saved in storage
 	}
 
+	//fetchThread() {}
+
 	add(board, thread) {
-/*
-		const watchListItemHtml = uploaditem({ data });
-		.insertAdjacentHTML('beforeend', uploadItemHtml);
-		const fileElem = this.fileUploadList.lastChild;
-		const lastClose = fileElem.querySelector('.close');
+		console.log('add');
+		//todo: = fetchThread();
+		//todo: dedup/prevent dup
+		//todo: push to watchListSet
+		//todo: modify watchListItemHtml to highlight/bold, if already in selected thread
+		const watchListItemHtml = watchedthread({ watchedthread: { board: 'test', subject: 'testing 123', postId: 1 } });
+		this.threadWatcher.insertAdjacentHTML('beforeend', watchListItemHtml);
+		const watchedThreadElem = this.threadWatcher.lastChild;
+		const lastClose = watchedThreadElem.querySelector('.close');
 		lastClose.addEventListener('click', () => {
-			this.removeFile(fileElem, file.name, file.size);
+			watchedThreadElem.remove();
+			this.remove(board, thread);
 		});
-*/
 	}
 
 	remove(board, thread) {
+		console.log('remove');
 //
 	}
 
 	clear() {
+		console.log('clear');
 		watchList = new Set();
 		watchListInput.value = '';
 		setLocalStorage('watchlist', '[]');
