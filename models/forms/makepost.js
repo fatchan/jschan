de9b82ebad474768452302b84a51c8f52ceaebc6
Thomas Lynch <tom@69420.me>
2022-01-19 15:21:10 +1100

    close #419

--- models/forms/makepost.js
+++ models/forms/makepost.js
@@ -53,16 +53,8 @@ module.exports = async (req, res, next) => {
 	let thread = null;
 	const { filterBanDuration, filterMode, filters, blockedCountries, threadLimit, ids, userPostSpoiler,
 		lockReset, captchaReset, pphTrigger, tphTrigger, tphTriggerAction, pphTriggerAction,
-		maxFiles, sageOnlyEmail, forceAnon, replyLimit, disableReplySubject,
+		sageOnlyEmail, forceAnon, replyLimit, disableReplySubject,
 		captchaMode, lockMode, allowedFileTypes, customFlags, geoFlags, fileR9KMode, messageR9KMode } = res.locals.board.settings;
-	if (res.locals.numFiles > maxFiles) {
-		await deleteTempFiles(req).catch(e => console.error);
-		return dynamicResponse(req, res, 400, 'message', {
-			'title': 'Too many files',
-			'message': `Max files per post on this board is ${maxFiles}`,
-			'redirect': req.headers.referer
-		});
-	}
 	if (res.locals.permLevel >= 4
 		&& res.locals.country
 		&& blockedCountries.includes(res.locals.country.code)) {
