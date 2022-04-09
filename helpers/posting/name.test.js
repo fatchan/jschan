ca74778796bd4780f3aa0ac22def51715ad473b1
Thomas Lynch <tom@69420.me>
2022-04-10 09:29:54 +1000

    "test:coverage" script, and improve tripcode test coverage

--- helpers/posting/name.test.js
+++ helpers/posting/name.test.js
@@ -3,6 +3,7 @@ const Permission = require('../permission.js')
 const Permissions = require('../permissions.js')
 const ROOT = new Permission();
 ROOT.setAll(Permission.allPermissions);
+const NO_PERMISSION = new Permission();
 
 describe('name/trip/capcode handler', () => {
 
@@ -17,12 +18,14 @@ describe('name/trip/capcode handler', () => {
 		{ in: 'test#12345', out: { name: 'test', tripcode: '!CSZ6G0yP9Q', capcode: null } },
 		{ in: '#12345', out: { name: 'Anon', tripcode: '!CSZ6G0yP9Q', capcode: null } },
 		{ in: '#12345## Admin', out: { name: 'Anon', tripcode: '!CSZ6G0yP9Q', capcode: '## Admin' } },
+		{ in: '###########', perm: NO_PERMISSION, out: { name: 'Anon', tripcode: '!!AR2Fv1lg4=', capcode: null } },
+		{ in: '##test', out: { name: 'Anon', tripcode: '!!4XzKAGoCE=', capcode: null } },
 		{ in: 'test#12345## Admin', out: { name: 'test', tripcode: '!CSZ6G0yP9Q', capcode: '## Admin' } },
 	];
 
 	for(let i in cases) {
 		test(`should contain ${cases[i].out.capcode} for an input of ${cases[i].in}`, async () => {
-			const output = await name(cases[i].in, ROOT, {forceAnon: false, defaultName: 'Anon'}, 'a', {a:ROOT}, 'b')
+			const output = await name(cases[i].in, (cases[i].perm || ROOT), {forceAnon: false, defaultName: 'Anon'}, 'a', {a:ROOT}, 'b')
 			expect(output).toStrictEqual(cases[i].out)
 		});
 	}
