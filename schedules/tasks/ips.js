0461842d148ec0b044f2cbe0886638b7fc53cbbe
Thomas Lynch <tom@69420.me>
2022-06-04 13:59:14 +1000

    Make IP pruning schedule also apply to modlogs Update CHANGELOG

--- schedules/tasks/ips.js
+++ schedules/tasks/ips.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Mongo = require(__dirname+'/../../db/db.js')
-	, { Posts } = require(__dirname+'/../../db/')
+	, { Posts, Modlogs } = require(__dirname+'/../../db/')
 	, config = require(__dirname+'/../../lib/misc/config.js')
 	, { createHash, randomBytes } = require('crypto')
 	, timeUtils = require(__dirname+'/../../lib/converter/timeutils.js');
@@ -9,24 +9,20 @@ const Mongo = require(__dirname+'/../../db/db.js')
 module.exports = {
 
 	func: async (days) => {
+
+		//make a mongoid set to the date before which we prunt IPs on, because _id holds date and is always indexed
 		const beforeDate = new Date();
 		beforeDate.setDate(beforeDate.getDate() - (days || config.get.pruneIps));
 		const beforeDateMongoId = Mongo.ObjectId.createFromTime(Math.floor(beforeDate.getTime()/1000));
+
+		//use random secret for this schedule run and generate the bulkwrite for pruning IP
 		const tempIpHashSecret = randomBytes(20).toString('base64');
-		const bulkWrites = [];
-		await Posts.db.find({
-			_id: {
-				$lte: beforeDateMongoId,
-			},
-			'ip.pruned': {
-				$ne: true
-			}
-		}).forEach(post => {
-			const randomIP = createHash('sha256').update(tempIpHashSecret + post.ip.cloak).digest('base64');
-			bulkWrites.push({
+		const mapBulkWrite = (document) => {
+			const randomIP = createHash('sha256').update(tempIpHashSecret + document.ip.cloak).digest('base64');
+			return {
 				updateOne: {
 					filter: {
-						_id: post._id,
+						_id: document._id,
 					},
 					update: {
 						$set: {
@@ -36,13 +32,32 @@ module.exports = {
 						}
 					}
 				}
-			});
+			};
+		};
+
+		const pruneQuery = {
+			'_id': {
+				'$lte': beforeDateMongoId,
+			},
+			'ip.pruned': {
+				'$ne': true
+			}
+		};
+
+		//No need for promise.all
+		[Posts, Modlogs].forEach(async (coll, i) => {
+			const bulkWrites = (await coll.db
+				.find(pruneQuery)
+				.toArray())
+				.map(mapBulkWrite);
+			if (bulkWrites.length > 0) {
+				await coll.db.bulkWrite(bulkWrites);
+				console.log(`Randomised ips on ${bulkWrites.length} ${i === 0 ? 'posts' : 'modlog entries'}`);
+			}
 		});
-		console.log(`Randomising ip on ${bulkWrites.length} posts`);
-		if (bulkWrites.length > 0) {
-			await Posts.db.bulkWrite(bulkWrites);
-		}
+
 	},
+
 	interval: timeUtils.DAY,
 	immediate: true,
 	condition: 'pruneIps',
