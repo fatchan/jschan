cc2eefc477f6971b20c6de7eec46610bd0db2bde
Thomas Lynch <tom@69420.me>
2022-08-18 23:46:58 +1000

    Bugfix showing correct amount of deleted asset/banner on board, deletedCount->modifiedCount because we are updating board object not deleting a document

--- models/forms/deleteassets.js
+++ models/forms/deleteassets.js
@@ -16,7 +16,7 @@ module.exports = async (req, res) => {
 	}));
 
 	//remove from db
-	const amount = await Boards.removeAssets(req.params.board, req.body.checkedassets).then(result => result.deletedCount);
+	const amount = await Boards.removeAssets(req.params.board, req.body.checkedassets).then(result => result.modifiedCount);
 
 	//update res locals assets in memory
 	res.locals.board.assets = res.locals.board.assets.filter(asset => {
