5f99ca5062fdf1e5d1ddb7d9e81436387c370ac0
Thomas Lynch <tom@69420.me>
2022-12-30 15:03:36 +1100

    Improve 0.11.0 migration add simple regex for handling ipv4/6 instead of assuming all ipv4 actually append 4/6 to existing cloaks set type 0 on all bans/posts before applying correct types to ensure all have at least some ip type

--- migrations/0.11.0.js
+++ migrations/0.11.0.js
@@ -3,6 +3,35 @@
 module.exports = async(db, redis) => {
 
 	console.log('changing post/report/ban ip type storage format');
+
+	await db.collection('posts').updateMany({
+		/* Make sure they all existing ones at least have a type 
+		in case the site uses "dont store raw ips" */
+	}, {
+		'$set': {
+			'ip.type': 0,
+		}
+	});	
+	await db.collection('posts').updateMany({
+		'ip.raw': /^([0-9]+(\.|$)){4}/
+	}, [{
+		'$set': {
+			'ip.type': 0,
+			'ip.cloak': {
+				$concat: ['$ip.cloak', '4']
+			}
+		}
+	}]);
+	await db.collection('posts').updateMany({
+		'ip.raw': /:/
+	}, [{
+		'$set': {
+			'ip.type': 1,
+			'ip.cloak': {
+				$concat: ['$ip.cloak', '6']
+			}
+		}
+	}]);
 	await db.collection('posts').updateMany({
 		'ip.cloak': /PRUNED$/
 	}, {
@@ -15,13 +44,6 @@ module.exports = async(db, redis) => {
 	}, {
 		'$set': {
 			'ip.type': 2,
-		}
-	});
-	await db.collection('posts').updateMany({
-		'ip.cloak': /IP[46]*$/
-	}, {
-		'$set': {
-			'ip.type': 0, //wont try and figure out migrating ipv4 vs 6
 		}
 	});
 	await db.collection('posts').updateMany({}, {
@@ -32,6 +54,34 @@ module.exports = async(db, redis) => {
 	});
 
 	await db.collection('bans').updateMany({
+		/* Make sure they all existing ones at least have a type 
+		in case the site uses "dont store raw ips" */
+	}, {
+		'$set': {
+			'ip.type': 0,
+		}
+	});
+	await db.collection('bans').updateMany({
+		'ip.raw': /^([0-9]+(\.|$)){4}/
+	}, [{
+		'$set': {
+			'ip.type': 0,
+			'ip.cloak': {
+				$concat: ['$ip.cloak', '4']
+			}
+		}
+	}]);
+	await db.collection('bans').updateMany({
+		'ip.raw': /:/
+	}, [{
+		'$set': {
+			'ip.type': 1,
+			'ip.cloak': {
+				$concat: ['$ip.cloak', '6']
+			}
+		}
+	}]);
+	await db.collection('bans').updateMany({
 		'ip.cloak': /PRUNED$/
 	}, {
 		'$set': {
@@ -43,13 +93,6 @@ module.exports = async(db, redis) => {
 	}, {
 		'$set': {
 			'ip.type': 2,
-		}
-	});
-	await db.collection('bans').updateMany({
-		'ip.cloak': /IP[46]*$/
-	}, {
-		'$set': {
-			'ip.type': 0,
 		}
 	});
 
