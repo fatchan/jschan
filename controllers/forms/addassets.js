a4927c86a162e3f391d62796b5874b26ec799280
Thomas Lynch <tom@69420.me>
2023-01-18 21:08:08 +1100

    Convert addflags and addassets to schema formhandler, was still on old form handling code

--- controllers/forms/addassets.js
+++ controllers/forms/addassets.js
@@ -3,10 +3,9 @@
 const addAssets = require(__dirname+'/../../models/forms/addassets.js')
 	, dynamicResponse = require(__dirname+'/../../lib/misc/dynamic.js')
 	, deleteTempFiles = require(__dirname+'/../../lib/file/deletetempfiles.js')
-	, config = require(__dirname+'/../../lib/misc/config.js');
+	, config = require(__dirname+'/../../lib/misc/config.js')
+	, { checkSchema, numberBody } = require(__dirname+'/../../lib/input/schema.js');
 
-//almost a copy of banners code, since it can be handled the same. maybe refactor both into 1 with a "type" arg or something
-//or allowing 2 types to accommodate flags too where they are named (not the object.keys & .values use in manageassets template)
 module.exports = {
 
 	//paramConverter: paramConverter({}),
@@ -14,15 +13,12 @@ module.exports = {
 	controller: async (req, res, next) => {
 
 		const { globalLimits } = config.get;
-		const errors = [];
 
-		if (res.locals.numFiles === 0) {
-			errors.push('Must provide a file');
-		} else if (res.locals.numFiles > globalLimits.assetFiles.max) {
-			errors.push(`Exceeded max asset uploads in one request of ${globalLimits.assetFiles.max}`);
-		} else if (res.locals.board.assets.length+res.locals.numFiles > globalLimits.assetFiles.total) {
-			errors.push(`Total number of assets would exceed global limit of ${globalLimits.assetFiles.total}`);
-		}
+		const errors = await checkSchema([
+			{ result: res.locals.numFiles === 0, expected: false, blocking: true, error: 'Must provide a file' },
+			{ result: numberBody(res.locals.numFiles, 0, globalLimits.assetFiles.max), expected: true, error: `Exceeded max asset uploads in one request of ${globalLimits.assetFiles.max}` },
+			{ result: numberBody(res.locals.board.assets.length+res.locals.numFiles, 0, globalLimits.assetFiles.max), expected: true, error: `Total number of assets would exceed global limit of ${globalLimits.assetFiles.total}` },
+		]);
 
 		if (errors.length > 0) {
 			await deleteTempFiles(req).catch(console.error);
