6016dac3ba7d576a010ef506bfdf72854050277c
Thomas Lynch <tom@69420.me>
2023-01-30 21:21:23 +1100

    script to extract roughly keys from a pug template with all the __() already added

---
+++ tools/viewstrings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# roughly extract all translation calls used in pug view file into key: value
+grep -ri '__' $1 \
+	| sed 's/__/\n/g' \
+	| grep -Po "(?<=')[^',]+(?=')" \
+	| sort \
+	| uniq \
+	| awk '{ print "\""$0"\""": ""\""$0"\"""," }'
