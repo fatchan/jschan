bd31ad8b501633c9938754e8856ca151b0074554
Thomas Lynch <tom@69420.me>
2023-02-04 22:46:32 +1100

    change edited post "hidden user" to be null and translate in post

--- migrations/1.0.0.js
+++ migrations/1.0.0.js
@@ -1,6 +1,14 @@
 'use strict';
 
 module.exports = async(db, redis) => {
+	console.log('Updating edited posts username property for "hidden users" to support localisation');
+	await db.collection('posts').updateMany({
+		'edited.username': 'Hidden User',
+	}, {
+		'$set': {
+			'edited.username': null,
+		},
+	});	
 	console.log('Updating db for language settings');
 	await db.collection('globalsettings').updateOne({ _id: 'globalsettings' }, {
 		'$set': {
