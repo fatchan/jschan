14ed2df69bc474f507207bbd02bb68377e8e4430
Thomas Lynch <tom@69420.me>
2023-05-07 14:54:53 +1000

    Remove some unnecessary settings changes from 1.0.5 migration

--- migrations/1.0.5.js
+++ migrations/1.0.5.js
@@ -4,15 +4,9 @@ module.exports = async(db, redis) => {
 	console.log('Updating db for language settings, fixes broken 1.0.0 migration');
 	await db.collection('globalsettings').updateOne({ _id: 'globalsettings' }, {
 		'$set': {
-			'language': 'en-GB',
 			'boardDefaults.language': 'en-GB',
 		},
 	});
-	await db.collection('boards').updateMany({}, {
-		'$set': {
-			'settings.language': 'en-GB',
-		},
-	});	
 
 	console.log('Clearing globalsettings cache');
 	await redis.deletePattern('globalsettings');
