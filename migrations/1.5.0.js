7b29f0646758d484fbb8e81b67c987627c8310a8
Thomas Lynch <tom@69420.me>
2024-05-04 18:37:46 +1000

    Update migration to also set global: false for completeness update version and migrateVersion in package.json

--- migrations/1.5.0.js
+++ migrations/1.5.0.js
@@ -2,12 +2,22 @@
 
 module.exports = async(db) => {
 
-	console.log('Updating all bans with board: null to be global true');
+	console.log('Updating all bans to have new global true/false flag');
 	await db.collection('bans').updateMany({
 		board: null,
 	}, {
 		'$set': {
 			'global': true,
+		},
+	});
+
+	await db.collection('bans').updateMany({
+		board: {
+			'$ne': null,
+		},
+	}, {
+		'$set': {
+			'global': false,
 		},
 	});
 
