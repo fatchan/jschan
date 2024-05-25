43e6b6d38ffdafeb5bd174ad659c4be9eabd8638
Thomas Lynch <tom@69420.me>
2024-05-25 15:50:42 +1000

    Add missing permission imports

--- models/forms/removebans.js
+++ models/forms/removebans.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const { Bans } = require(__dirname+'/../../db/');
+const { Bans } = require(__dirname+'/../../db/')
+	, { Permissions } = require(__dirname+'/../../lib/permission/permissions.js');
 
 module.exports = async (req, res) => {
 
 	const showGlobal = res.locals.permissions.get(Permissions.VIEW_BOARD_GLOBAL_BANS);
-	const bansBoard = req.params.board ? showGlobal ? req.parms.board : { '$eq': req.params.board } : null;
+	const bansBoard = req.params.board ? (showGlobal ? req.parms.board : { '$eq': req.params.board }) : null;
 	return Bans.removeMany(bansBoard, req.body.checkedbans).then(result => result.deletedCount);
 
 };
