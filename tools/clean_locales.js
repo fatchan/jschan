1085ff37bd8ee178e9916e38186691e954484d8d
Thomas Lynch <tom@69420.me>
2024-06-05 11:31:28 +1000

    Update locale cleaning tool to inherit missing keys from en-GB Update locales

--- tools/clean_locales.js
+++ tools/clean_locales.js
@@ -1,11 +1,21 @@
-const locales = ['en-GB', 'pt-PT', 'pt-BR', 'ru-RU', 'it-IT'];
+const fs = require('fs')
+	, locales = ['en-GB', 'pt-PT', 'pt-BR', 'ru-RU', 'it-IT']
+	, enGBData = require(__dirname+`/../locales/en-GB.json`);
 
 locales.forEach(locale => {
-    const sortedEntries = Object.entries(require(__dirname+`/../locales/${locale}.json`))
-        .sort((a, b) => a[0].localeCompare(b[0]))
-        .reduce((acc, e) => {
-            acc[e[0]] = e[1];
-            return acc;
-        }, {});
-    require('fs').writeFileSync(__dirname+`/../locales/${locale}.json`, JSON.stringify(sortedEntries, null, '\t'));
+	const localeData = require(__dirname+`/../locales/${locale}.json`);
+	// Take any missing values from en-GB
+	for (const key in enGBData) {
+		if (!localeData[key]) {
+			localeData[key] = enGBData[key];
+		}
+	}
+	// Sort and write updated data
+	const sortedEntries = Object.entries(localeData)
+		.sort((a, b) => a[0].localeCompare(b[0]))
+		.reduce((acc, [key, value]) => {
+			acc[key] = value;
+			return acc;
+		}, {});
+	fs.writeFileSync(__dirname+`/../locales/${locale}.json`, JSON.stringify(sortedEntries, null, '\t'));
 });
