f3d5d159d15218333f2ef2c5add358232e221e36
Thomas Lynch <tom@69420.me>
2024-06-10 22:22:33 +1000

    Add tests for 2fa enforcement Fix some bugs in twofactor tests, deletepattern would delete session, now only deleting the twofactor_success entry

--- test/twofactor.js
+++ test/twofactor.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch')
 	, OTPAuth = require('otpauth')
-	, redis = require(__dirname+'/../lib/redis/redis.js');
+	, redis = require(__dirname+'/../lib/redis/redis.js')
+	, globalSettings = require(__dirname+'/res/globalsettings.js');
 
 module.exports = () => describe('test two factor authentication', () => {
 
@@ -38,6 +39,43 @@ module.exports = () => describe('test two factor authentication', () => {
 		});
 		const text = await response.text();
 		expect(text).toContain('<a href="/twofactor.html"');
+	});
+
+	test('make sure not force redirected to twofactor page',  async () => {
+		const response = await fetch('http://localhost/account.html', {
+			headers: {
+				'cookie': sessionCookie,
+			},
+		});
+		expect((await response.text())).not.toContain('<span class="code">'); //Note: .not
+	});
+
+	test('enable account 2fa enforcement',  async () => {
+		const params = new URLSearchParams({
+			_csrf: csrfToken,
+			...globalSettings,
+			// force_action_twofactor: true,
+			force_account_twofactor: true
+		});
+		const response = await fetch('http://localhost/forms/global/settings', {
+			headers: {
+				'cookie': sessionCookie,
+			},
+			method: 'POST',
+			body: params,
+			redirect: 'manual',
+		});
+		expect(response.status).toBe(200);
+	});
+
+	test('make sure force redirected to twofactor page',  async () => {
+		const response = await fetch('http://localhost/account.html', {
+			headers: {
+				'cookie': sessionCookie,
+			},
+			// redirect: 'manual', //Note: allow to be redirected to /twofactor.html
+		});
+		expect((await response.text())).toContain('<span class="code">');
 	});
 	
 	test('load twofactor page and fetch secret',  async () => {
@@ -123,7 +161,7 @@ module.exports = () => describe('test two factor authentication', () => {
 	});
 
 	test('login as admin with correct 2fa',  async () => {
-		await redis.deletePattern('*'); //delete used 2fa code before trying to login again (same token at this point)
+		await redis.deletePattern('twofactor_success:admin:*'); //delete used 2fa code before trying to login again (same token at this point)
 		const params = new URLSearchParams();
 		params.append('username', 'admin');
 		params.append('password', process.env.TEST_ADMIN_PASSWORD);
@@ -140,6 +178,69 @@ module.exports = () => describe('test two factor authentication', () => {
 		const rawHeaders = response.headers.raw();
 		expect(rawHeaders['set-cookie']).toBeDefined();
 		expect(rawHeaders['set-cookie'][0]).toMatch(/^connect\.sid/);
+		// Set updated session cookie and token for 2fa enforcement
+		sessionCookie = rawHeaders['set-cookie'][0];
+		csrfToken = await fetch('http://localhost/csrf.json', { headers: { 'cookie': sessionCookie }})
+			.then(res => res.json())
+			.then(json => json.token);
+	});
+
+	test('enable action 2fa enforcement',  async () => {
+		const params = new URLSearchParams({
+			_csrf: csrfToken,
+			...globalSettings,
+			force_action_twofactor: true,
+			// force_account_twofactor: true
+		});
+		const response = await fetch('http://localhost/forms/global/settings', {
+			headers: {
+				'cookie': sessionCookie,
+			},
+			method: 'POST',
+			body: params,
+			redirect: 'manual',
+		});
+		expect(response.status).toBe(200);
+	});
+
+	test('settings change blocked when missing twofactor in body',  async () => {
+		const params = new URLSearchParams({
+			_csrf: csrfToken,
+			...globalSettings,
+			// force_action_twofactor: true,
+			// force_account_twofactor: true
+		});
+		const response = await fetch('http://localhost/forms/global/settings', {
+			headers: {
+				'cookie': sessionCookie,
+			},
+			method: 'POST',
+			body: params,
+			redirect: 'manual',
+		});
+		console.log((await response.text()));
+		expect(response.status).toBe(400);
+	});
+
+	test('settings change works with twofactor in body, and disabling 2fa enforcement',  async () => {
+		await redis.deletePattern('twofactor_success:admin:*'); //delete used 2fa code before trying to login again (same token at this point)
+		const params = new URLSearchParams({
+			_csrf: csrfToken,
+			...globalSettings,
+			// force_action_twofactor: true,
+			// force_account_twofactor: true
+			twofactor: twofactorToken,
+		});
+		const response = await fetch('http://localhost/forms/global/settings', {
+			headers: {
+				'cookie': sessionCookie,
+			},
+			method: 'POST',
+			body: params,
+			redirect: 'manual',
+		});
+		console.log((await response.text()));
+		expect(response.status).toBe(200);
 	});
 
 	test('login as admin again with correct 2fa (rejected for reuse)',  async () => {
@@ -198,7 +299,7 @@ module.exports = () => describe('test two factor authentication', () => {
 	});
 
 	test('change admin password with correct 2fa',  async () => {
-		await redis.deletePattern('*'); //delete used 2fa code before trying to login again (same token at this point)
+		await redis.deletePattern('twofactor_success:admin:*'); //delete used 2fa code before trying to login again (same token at this point)
 		const params = new URLSearchParams();
 		params.append('username', 'admin');
 		params.append('password', process.env.TEST_ADMIN_PASSWORD);
@@ -215,8 +316,8 @@ module.exports = () => describe('test two factor authentication', () => {
 			body: params,
 			redirect: 'manual',
 		});
-		redis.close();
+		await redis.close();
 		expect(response.status).toBe(200);
-	});	
+	});
 
 });
