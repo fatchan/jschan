9e94aa8f0d3d2a6e0b27c77028a662dd1030753b
Thomas Lynch <tom@69420.me>
2024-11-26 13:50:21 +1100

    fix migration, exclude webring boards and skip boards without an owner ref #571

--- migrations/1.7.1.js
+++ migrations/1.7.1.js
@@ -8,9 +8,14 @@ const { Permissions } = require(__dirname+'/../lib/permission/permissions.js')
 module.exports = async(db, redis) => {
 
 	console.log('Updating exsting board owners to have the updated markdown image bit in their board level perms');
-	const boards = await db.collection('boards').find().toArray();
+	const boards = await db.collection('boards').find({
+		webring: false,
+	}).toArray();
 	for (let b of boards) {
 		const boardOwner = b.owner;
+		if (!boardOwner) {
+			continue;
+		}
 		const boardOwnerPerm = new Permission(b.staff[boardOwner].permissions.toString('base64'));
 		boardOwnerPerm.set(Permissions.USE_MARKDOWN_IMAGE, true);
 		//write the change to DB
